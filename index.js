1644cde533a88309fd1b4f21401b0c6ae38061fb
Mirigi95 <56714464+Mirigi95@users.noreply.github.com>
2024-07-29 17:59:56 +0300

    Update index.js
    ver

--- index.js
+++ index.js
@@ -99,7 +99,4 @@ app.get('/arbitrage', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
-module.exports = app; 
+module.exports = app; // Export the app for Vercel
